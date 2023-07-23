7307357de2f43ab600cf5f7ddfa42280a3d72d1c
Filipe André <flipeandre12@gmail.com>
2023-07-23 14:10:36 -0300

    fix: corrige com uso de await.

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 import decide from './Middleware/decision.mjs'
 import readlineSync from 'readline-sync';
-import playMp3 from './player/audioPlayer.mjs'
 
-let text = readlineSync.question('Digite a mensagem: ');
+while (true){
+  let text = readlineSync.question('Digite a mensagem: ');
   console.log(text);
-  decide(text);
+  await decide(text);
+};
   
   
