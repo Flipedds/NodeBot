4f8b9aa06271db623db82417bdb0fdcaba8050af
Filipe André <flipeandre12@gmail.com>
2023-07-23 20:31:14 -0300

    fix: add comentário.

--- tests/converter.test.js
+++ tests/converter.test.js
@@ -1,5 +1,5 @@
 import parser from '../Middleware/converter.mjs';
-
+// para comparar arrays usar toEqual
 test('get Array of saudacao', () => {
     expect(parser('intents/yamls/saudacao.yaml')).toEqual(["oi","ola","como vai?","eae","tudo bem?"]);
 });
