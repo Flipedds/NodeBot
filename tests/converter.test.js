df2332113f91aaae2ba33925fd7e8f5f1c2e08b0
Filipe André <flipeandre12@gmail.com>
2023-07-23 20:22:40 -0300

    feat: add teste case.

--- tests/converter.test.js
+++ tests/converter.test.js
@@ -3,3 +3,8 @@ import parser from '../Middleware/converter.mjs';
 test('get Array of saudacao', () => {
     expect(parser('intents/yamls/saudacao.yaml')).toEqual(["oi","ola","como vai?","eae","tudo bem?"]);
 });
+
+test('get Array of despedida', () => {
+    expect(parser('intents/yamls/despedida.yaml')).toEqual(["ate mais","tchau","bye"]);
+});
+
